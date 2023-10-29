970ac22d183c02b8e92d9cc010c523801c580bb0
Найденов Вячеслав <147939307+slava214365@users.noreply.github.com>
2023-10-29 20:40:26 +0300

    Update ЛР2.2.py

--- ЛР2/ЛР2.2.py
+++ ЛР2/ЛР2.2.py
@@ -5,7 +5,7 @@ increase = 0.03  # Ежемесячный рост цен
 rate = 1 + increase # Коэффициент роста цен
 money_capital = 0
 
-for month in range(10):
+for month in range(months):
     money_capital += spend * rate ** month - salary
 
 money_capital = int(money_capital)
